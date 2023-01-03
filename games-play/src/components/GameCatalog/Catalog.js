8b7f245292ebe7a3582d28f3877ea2d6ff2c75ed
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-01-03 22:59:47 +0200

    Show no games message

--- games-play/src/components/GameCatalog/Catalog.js
+++ games-play/src/components/GameCatalog/Catalog.js
@@ -15,10 +15,12 @@ function Catalog() {
   return (
     <section id="catalog-page">
       <h1>All Games</h1>
- 
-      { games.map(x => <GameCard game={x} />) }
 
-      <h3 className="no-articles">No articles yet</h3>
+      {games.length > 0
+        ? games.map(x => <GameCard key={x._id} game={x} />)
+        : <h3 className="no-articles">No articles yet</h3>
+      }
+
     </section>
   )
 }
